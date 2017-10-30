a9086c0369ccc6aaca89fb1381b3ab4ca1984c47
MartinMyr <martin.myrmarker@gmail.com>
2017-10-30 21:54:53 +0100

    Add mediaq
    Fixed code order, added mediaquery,
    Completed button element.

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 var listOfProducts;
 
 
-
 fetch("./products.json")
 .then(function(response) {
     return response.json();
@@ -12,64 +11,78 @@ fetch("./products.json")
     createUIFromLoadedProducts();
 });
 
-/** Uses the loaded products data to create a visible product list on the website */
-    function createUIFromLoadedProducts() {
-    //Skapar diven där allt ska skrivas ut
-    var content = document.createElement("div");
-   
 
+    function createUIFromLoadedProducts() {
+   
+   
+        //Skapar div där allt ska skrivas ut
+        var content = document.createElement("div");
+   
+        //Loopar ut infot ur product.json
         for (var i = 0; i < listOfProducts.length; i++ ){
         var productCard = createProductCard(listOfProducts[i]);
         content.appendChild(productCard);
 
     }
-    document.body.appendChild(content);
-    /* Check your console to see that the products are stored in the listOfProducts varible */
+        document.body.appendChild(content);
+
   
 
 
 }
 
     function createProductCard (listOfProducts) {
-    var productCard = document.createElement("div")
-    productCard.className = "productCard";
-
-    var getProductTitle = document.createElement("h2");
-    getProductTitle.innerText = listOfProducts.title;
-    productCard.appendChild(getProductTitle);
-
-    var getProductDescription = document.createElement("h4");
-    getProductDescription.innerText = listOfProducts.description;
-    productCard.appendChild(getProductDescription);
-
-    var getProductImage = document.createElement("img");
-    getProductImage.src = "assets/" + listOfProducts.image;
-    productCard.appendChild(getProductImage);
-
-    var getProductPrice = document.createElement("h4");
-    getProductPrice.innerText = listOfProducts.price;
-    productCard.appendChild(getProductPrice);
-
-    var buttonElement = document.createElement("button");
-    buttonElement.innerText = "Add to cart";
-    productCard.appendChild(buttonElement);
+        var productCard = document.createElement("div")
+        productCard.className = "productCard";
 
 
-     button  = 0;
+        
+        var getProductTitle = document.createElement("h1");
+        getProductTitle.innerText = listOfProducts.title;
+        
+
+
     
-     var span = document.querySelector("span");
-      buttonElement.onclick = function() {
-         button += 1;
-         span.innerText = button;
-      }
+        var getProductDescription = document.createElement("h4");
+        getProductDescription.innerText = listOfProducts.description;
+        
 
-    return productCard;
+        var getProductImage = document.createElement("img");
+        getProductImage.src = "assets/" + listOfProducts.image;
+        
+
+        var getProductPrice = document.createElement("h4");
+        getProductPrice.innerText = listOfProducts.price;
+        
+
+        var buttonElement = document.createElement("button");
+        var cartbuttonText = document.createTextNode("Lägg till i kundvagnen");
+        buttonElement.innerHTML = '<i class="fa fa-shopping-cart fa-2x" aria-hidden="true"></i>'+ '\xa0' + '\xa0' + '\xa0' + 'Lägg till i kundvagnen' ;
+        
+
+        productCard.appendChild(getProductTitle);
+        productCard.appendChild(getProductDescription);
+        productCard.appendChild(getProductImage);
+        productCard.appendChild(getProductPrice);
+        productCard.appendChild(buttonElement);
+        
+       
+         // Tilldelar värdet 0 till button, vid klick blir det +1 som visas i span. Alert  ger ett meddelande om vilken telefon man köper.
+
+         button  = 0;
+    
+            var span = document.querySelector("span");
+        buttonElement.onclick = function() {
+            
+            alert("Du har lagt till en" + '\xa0'  +  listOfProducts.title   + '\xa0' +  "i kundvagnen") ; 
+            button += 1;
+            span.innerText = button;
+            
+
+         }
 
 
-}
-
-  
-  
+            return productCard;
 
 
- 
+    }
